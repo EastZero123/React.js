818b30a500578a991adaa61c4dd7d91ae4280270
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-07-06 21:38:13 +0900

    Add files via upload

--- react-review-router/src/App.js
+++ react-review-router/src/App.js
@@ -1,5 +1,26 @@
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
--- react-review-router/src/pages/AllMeetups.js
+++ react-review-router/src/pages/AllMeetups.js
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  fetch("https://react-http-fa2fe-default-rtdb.firebaseio.com/meetups.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setIsLoading(false);
+      setLoadedMeetups(data);
+    });
+
+  if (!isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={DUMMY_DATA} />
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
