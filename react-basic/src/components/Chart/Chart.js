748954106d44bc756cba000adc6300ffd3d6d2ef
EastZero123 <cdy1005p@naver.com>
2022-07-20 16:20:15 +0900

    CH05 finish

---
+++ react-basic/src/components/Chart/Chart.js
@@ -0,0 +1,14 @@
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar />
+      ))}
+    </div>
+  );
+};
+
+export default Chart();
---
+++ react-basic/src/components/Chart/ChartBar.js
@@ -0,0 +1,5 @@
+import "./ChartBar.js";
+
+const ChartBar = (props) => {};
+
+export default ChartBar;
