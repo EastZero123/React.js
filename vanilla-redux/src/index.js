27d89afcaf174538d2287e67d694c8707a4f2372
EastZero123 <cdy1005p@naver.com>
2022-07-25 22:18:51 +0900

    understanding redux

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,10 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
